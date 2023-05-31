4482670dd632af792152413a061f622253b8cca7
weward <dev.weward@gmail.com>
2023-05-31 15:04:21 +0800

    Laravel Fortify and migration for admins+companies

---
+++ database/migrations/2014_10_11_000000_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('contact_no')->nullable();
+            $table->boolean('active')->default(1);
+            $table->boolean('active_subsscription')->default(0);
+            $table->dateTime('subscribed_at')->nullable();
+            $table->timestamps();
+
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
